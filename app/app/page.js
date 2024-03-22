66fc21d399d1a66d9fa9c9fce130b9fbf5e928ad
evildeedz <sahilverma1311@protonmail.com>
2024-03-22 13:46:29 +0530

    App page

--- app/app/page.js
+++ app/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { getTranslations } from "@/utils/getTranslation";
 import axios from "axios";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import TitleTags from "../components/Title";
@@ -282,59 +283,75 @@ function App() {
 
         await axios.get("https://api.starfiles.co/bundle_id/" + data.bundle_id).then((bundleData) => {
           let bundle_id_data = bundleData.data;
-          setDownloadLinks(<></>);
-          document.getElementById("download_links").innerHTML = "";
-          for (var version in bundle_id_data.apps) {
-            if (bundle_id_data.apps.hasOwnProperty(version)) {
-              str = "";
-              for (i = 0; i < bundle_id_data.apps[version].length; i++) {
-                str +=
-                  `<div class="flex items-center justify-between">
-                                <div>
-                                    <p class="text-l">` +
-                  bundle_id_data.apps[version][i].clean_ipa_filename +
-                  `</p>
-                                    <p class="text-xs">Uploaded ` +
-                  timeDifference(bundle_id_data.apps[version][i].upload_time) +
-                  ` • ` +
-                  bundle_id_data.apps[version][i].views +
-                  ` Views • ` +
-                  bundle_id_data.apps[version][i].downloads +
-                  ` Downloads</p>
-                                </div>
-                                <div class="flex flex-col md:flex-row text-right gap-1">` +
-                  (document.cookie.indexOf("udid=") === -1
-                    ? `<a href="/purchase" type="button" class="text-white bg-[#<?php echo $theme['secondary'];?>] hover:bg-[#0096C7] md:font-medium text-xs rounded-lg py-2.5 text-center w-[5rem]">Install</a>`
-                    : `<a href="/signer#` +
-                      bundle_id_data.apps[version][i].id +
-                      `" type="button" class="text-white bg-[#<?php echo $theme['secondary'];?>] hover:bg-[#0096C7] md:font-medium text-xs rounded-lg py-2.5 text-center w-[5rem]">Install</a>`) +
-                  `<a href="//starfiles.co/file/` +
-                  bundle_id_data.apps[version][i].id +
-                  `" type="button" class="text-white bg-[#0096C7] hover:bg-[#<?php echo $theme['primary'];?>] md:font-medium text-xs rounded-lg py-2.5 text-center w-[5rem]">Download</a>
-                                </div>
-                            </div>`;
-                if (i == bundle_id_data.apps.len && id != bundle_id_data.apps[version][i].id)
-                  window.location.href = "/app/" + bundle_id_data.apps[version][i].id;
-              }
-              document.getElementById("download_links").innerHTML +=
-                '<h3 class="text-xl font-semibold mt-4">' +
-                version +
-                '</h3><div class="grid grid-cols-1 gap-1">' +
-                str +
-                "</div>";
-            }
-          }
-          document.getElementById("views").innerHTML =
-            bundle_id_data.views > 1000 ? Math.round(bundle_id_data.views / 1000) + "K" : bundle_id_data.views;
-          document.getElementById("downloads").innerHTML =
+          setDownloadLinks(
+            <>
+              {Object.keys(bundle_id_data?.apps)?.map((version, versionIndex) => {
+                if (bundle_id_data.apps.hasOwnProperty(version))
+                  return (
+                    <>
+                      <h3 key={versionIndex} class="text-xl font-semibold mt-4">
+                        {version}
+                      </h3>
+                      <div class="grid grid-cols-1 gap-1">
+                        {bundle_id_data.apps[version].map((app, index) => {
+                          if (index == bundle_id_data.apps.len && id != app.id)
+                            // TODO make /app?id to /app/id
+                            window.location.href = "/app?id=" + app.id;
+                          return (
+                            <div className="flex items-center justify-between" key={index}>
+                              <div>
+                                <p className="text-l">{app.clean_ipa_filename}</p>
+                                <p className="text-xs">
+                                  Uploaded {timeDifference(app.upload_time)} • {app.views} Views • {app.downloads}{" "}
+                                  Downloads
+                                </p>
+                              </div>
+                              <div className="flex flex-col md:flex-row text-right gap-1">
+                                {document.cookie.indexOf("udid=") === -1 ? (
+                                  <Link
+                                    href="/purchase"
+                                    type="button"
+                                    class="text-white bg-secondary hover:bg-[#0096C7] md:font-medium text-xs rounded-lg py-2.5 text-center w-[5rem]"
+                                  >
+                                    Install
+                                  </Link>
+                                ) : (
+                                  <Link
+                                    href={"/signer#" + app.id}
+                                    type="button"
+                                    class="text-white bg-secondary hover:bg-[#0096C7] md:font-medium text-xs rounded-lg py-2.5 text-center w-[5rem]"
+                                  >
+                                    Install
+                                  </Link>
+                                )}
+                                <a
+                                  href={"https://starfiles.co/file/" + app.id}
+                                  type="button"
+                                  class="text-white bg-[#0096C7] hover:bg-primary md:font-medium text-xs rounded-lg py-2.5 text-center w-[5rem]"
+                                >
+                                  Download
+                                </a>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </>
+                  );
+                else return <></>;
+              })}
+            </>
+          );
+          setViews(bundle_id_data.views > 1000 ? Math.round(bundle_id_data.views / 1000) + "K" : bundle_id_data.views);
+          setDownloads(
             bundle_id_data.downloads > 1000
               ? Math.round(bundle_id_data.downloads / 1000) + "K"
-              : bundle_id_data.downloads;
+              : bundle_id_data.downloads
+          );
         });
       }
     } catch (err) {
-      alert(err.message);
-      console.error(err.message);
+      console.log(err.message);
     }
   };
 
@@ -434,27 +451,31 @@ function App() {
                     </button>
                   </div>
                   <div class="px-6 pb-6 md:h-[75vh] h-[100vh] overflow-scroll" id="download_links">
-                    <div class="w-4 h-4 mx-auto my-8">
-                      <div role="status">
-                        <svg
-                          aria-hidden="true"
-                          class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#0096C7]"
-                          viewBox="0 0 100 101"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                            fill="currentColor"
-                          />
-                          <path
-                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                            fill="currentFill"
-                          />
-                        </svg>
-                        <span class="sr-only">Loading...</span>
+                    {downloadLinks ? (
+                      downloadLinks
+                    ) : (
+                      <div class="w-4 h-4 mx-auto my-8">
+                        <div role="status">
+                          <svg
+                            aria-hidden="true"
+                            class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#0096C7]"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="currentColor"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="currentFill"
+                            />
+                          </svg>
+                          <span class="sr-only">Loading...</span>
+                        </div>
                       </div>
-                    </div>
+                    )}
                   </div>
                 </div>
               </div>
