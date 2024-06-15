21e653ffa4ed8b3dd5b61fb37762a043ef6fc59f
Parsa Yazdani <3parsa3@gmail.com>
2024-06-15 21:02:37 +1000

    Update page.js

--- app/purchase/page.js
+++ app/purchase/page.js
@@ -151,7 +151,7 @@ function Purchase({ params, searchParams }) {
             )}
           </p>
           <a
-            className="mt-6 text-white bg-primary hover:bg-[#023E8A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mt-6 text-white bg-primary hover:bg-[#023E8A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-block"
             href="https://buy.stripe.com/cN29E90wo3WvdQQ6oz"
           >
             Install Now
