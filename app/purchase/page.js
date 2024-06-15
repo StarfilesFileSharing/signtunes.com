67a23853656dccd1ef6fe7f83852adf8a6d9f8d5
Parsa Yazdani <3parsa3@gmail.com>
2024-06-15 20:32:46 +1000

    remove gtag conversion tracker

--- app/purchase/page.js
+++ app/purchase/page.js
@@ -93,28 +93,6 @@ function Purchase({ params, searchParams }) {
     if (cookie("referral") !== referral && cookie("referral")) window.location.href = "/purchase/" + cookie("referral");
   };
 
-  function gtag_report_conversion(url) {
-    var callback = function () {
-      if (typeof url != "undefined") {
-        window.location = url;
-      }
-    };
-    window.dataLayer.push({
-      event: "conversion",
-      sendTo: "AW-875730316/wr5HCPKntIcYEIyryqED",
-      value: 15.99,
-      currency: "USD",
-      eventCallback: callback,
-    });
-    // gtag("event", "conversion", {
-    //   send_to: "AW-875730316/wr5HCPKntIcYEIyryqED",
-    //   value: 15.99,
-    //   currency: "USD",
-    //   event_callback: callback,
-    // });
-    return false;
-  }
-
   return (
     <>
       <head>
@@ -131,9 +109,6 @@ function Purchase({ params, searchParams }) {
             <button
               type="submit"
               className="mt-6 text-white bg-primary hover:bg-[#023E8A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4"
-              onClick={() => {
-                gtag_report_conversion();
-              }}
             >
               <span>{translationList?.purchase}</span>&nbsp;$
               {`${
@@ -182,9 +157,6 @@ function Purchase({ params, searchParams }) {
               <button
                 type="submit"
                 className="mt-6 text-white bg-primary hover:bg-[#023E8A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={() => {
-                  gtag_report_conversion();
-                }}
               >
                 Install Now
               </button>
@@ -199,9 +171,6 @@ function Purchase({ params, searchParams }) {
               <button
                 type="submit"
                 className="text-white bg-primary hover:bg-[#023E8A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={() => {
-                  gtag_report_conversion();
-                }}
               >
                 Pay with Crypto
               </button>
