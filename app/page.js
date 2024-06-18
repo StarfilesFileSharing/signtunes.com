ed946b6dd8858617c052d60fccae11fe4bdf5014
Parsa Yazdani <parsa@yazdani.au>
2024-06-19 02:16:33 +1000

    test

--- app/page.js
+++ app/page.js
@@ -139,7 +139,6 @@ export default function Home({ searchParams }) {
 
   return (
     <>
-      <Header searchParams={searchParams} />
       <main className="mx-5 md:mx-12 mt-5 grid grid-cols-4 mb-6 ">
         <div className="md:w-12/12 col-span-4 md:col-span-3 grid grid-cols-6">
           {alertsLoad && (
