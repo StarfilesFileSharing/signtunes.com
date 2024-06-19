9301c8d104a7065dfd3c517e174a0e419eb3ada4
Parsa Yazdani <3parsa3@gmail.com>
2024-06-19 13:52:37 +1000

    A/B test pricing

--- app/purchase/page.js
+++ app/purchase/page.js
@@ -152,7 +152,7 @@ function Purchase({ params, searchParams }) {
           </p>
           <a
             className="mt-6 text-white bg-primary hover:bg-[#023E8A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-block"
-            href="https://buy.stripe.com/cN29E90wo3WvdQQ6oz"
+            href={Math.random() < 0.33 ? "https://buy.stripe.com/cN29E90wo3WvdQQ6oz" : (Math.random() < 0.5 ? "https://buy.stripe.com/28oaId7YQgJh288dR3" : "https://buy.stripe.com/3csdUp3IA3WvfYY14i")}
           >
             Install Now
           </a>
