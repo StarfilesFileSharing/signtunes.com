1ac211692e2ce59d55d81f7f5cb00778980710c3
Kiefer <kieferlin1001@gmail.com>
2024-07-30 15:15:57 -0700

    Claim system

--- app/claim/page.js
+++ app/claim/page.js
@@ -10,14 +10,11 @@ function Claim({ searchParams }) {
   const { code } = searchParams;
   const [translationList, setTranslationList] = useState(null);
   const [codeInput, setCodeInput] = useState(code ? code : "");
+  const [showNext, setShowNext] = useState(code ? code : false);
   const [udid, setUdid] = useState("");
-  const [serialNumber, setSerialNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [showCode, setShowCode] = useState(true);
-  const [showIosUdid, setShowIosUdid] = useState(false);
-  const [showICloudUnlock, setShowICloudUnlock] = useState(false);
-  const [successMessage, setSuccessMessage] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [workingOnIt, setLoading] = useState("");
+  const [error, setError] = useState("");
+  const [allDone, setAllDone] = useState(false);
 
   useEffect(() => {
     // Get Translations
@@ -34,109 +31,32 @@ function Claim({ searchParams }) {
     }
   };
 
-  const checkCode = async () => {
-    try {
-      const response = await axios.get("https://api.starfiles.co/payments/check_code?check=true&code=" + codeInput);
-      let data = response.data;
-      setShowCode(false);
-      if (data.status) {
-        if (data.product === "signtunes_pro_1m" || data.product === "signtunes_pro_1y") {
-          setShowIosUdid(true);
-          setErrorMessage(null);
-        } else if (data.product === "signtunes_unlock") {
-          setShowICloudUnlock(true);
-          setErrorMessage(null);
-        } else {
-          setErrorMessage("Unkown Product");
-        }
-      } else {
-        setErrorMessage(data.message);
-      }
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
   const submitIosUdid = async () => {
+    console.log("submitting UDID");
+    setLoading(true);
     try {
-      const response = await axios.get(
-        "https://api.starfiles.co/payments/check_code?code=" + codeInput + "&udid=" + udid
-      );
-      let data = response.data;
-      setShowIosUdid(false);
-      if (data.status) {
-        setSuccessMessage(
-          <>
-            Download all these files. You won't be able to access this page again.
-            <br />
-            Distribution P12 (Recommended):{" "}
-            <Link href={`https://starfiles.co/file/${data.certificates.distribution}`} className="text-blue-500">
-              Download
-            </Link>
-            <br />
-            Wildcard Mobileprovision:{" "}
-            <Link href={`https://starfiles.co/file/${data.profiles.distribution_wildcard}`} className="text-blue-500">
-              Download
-            </Link>
-            <br />
-            Explicit Mobileprovision (Recommended):{" "}
-            <Link href={`https://starfiles.co/file/${data.profiles.distribution_explicit}`} className="text-blue-500">
-              Download
-            </Link>
-            <br />
-            <br />
-            Development P12:{" "}
-            <Link href={`https://starfiles.co/file/${data.certificates.development}`} className="text-blue-500">
-              Download
-            </Link>
-            <br />
-            Wildcard Mobileprovision:{" "}
-            <Link href={`https://starfiles.co/file/${data.profiles.development_wildcard}`} className="text-blue-500">
-              Download
-            </Link>
-            <br />
-            Explicit Mobileprovision (Recommended):{" "}
-            <Link href={`https://starfiles.co/file/${data.profiles.development_explicit}`} className="text-blue-500">
-              Download
-            </Link>
-          </>
-        );
-        setErrorMessage(null);
-      } else {
-        if (data.message === "Device on hold")
-          setErrorMessage(
-            <>
-              Your device is on hold. Redo this process when your device is signed. You can check your device status by
-              clicking <Link href="/device_status">here</Link>
-            </>
-          );
-        else setErrorMessage(data.message);
-      }
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
-  const submitSerialNumber = async () => {
-    try {
-      const response = await axios.get(
-        "https://api.starfiles.co/payments/check_code?code=" +
-          codeInput +
-          "&serial_number=" +
-          serialNumber +
-          "&email=" +
-          email
-      );
-      let data = response.data;
-      setShowICloudUnlock(false);
-      if (data.status) {
-        setSuccessMessage(<>Your device has been queued, you will be notified via email when it is ready.</>);
-        setErrorMessage(null);
-      } else {
-        setErrorMessage(data.message);
-      }
-    } catch (err) {
-      console.error(err.message);
+      fetch(`https://api2.starfiles.co/claim_gift_code/${codeInput}?udid=${udid}`, {
+        method: "POST"
+      }).then(async (r) => {
+        if (r.status !== 200 && r.status !== 203) {
+          setError(`Server returned status code ${r.status}`);
+          return;
+        } else {
+          const data = await r.json();
+          if (data.error) {
+            setError(data.error);
+            return;
+          } else {
+            setError("");
+            setAllDone(true);
+            setShowNext(false);
+          }
+        }
+      })
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -152,8 +72,8 @@ function Claim({ searchParams }) {
             {translationList?.claim_purchase}
           </h1>
         </div>
-        <div className="block rounded-xl p-8 shadow-xl bg-bright dark:text-black lg:mx-96 mb-12 dark:text-gray-900">
-          {showCode && (
+        <div className="block rounded-xl p-8 shadow-xl bg-bright lg:mx-96 mb-12 dark:text-gray-900">
+          {!showNext && (
             <div id="code">
               <h3 className="text-xl font-semibold">Code</h3>
               <p>{translationList?.code_prompt}</p>
@@ -169,13 +89,15 @@ function Claim({ searchParams }) {
                 className="block text-center w-[100%] rounded-md bg-primary hover:bg-[#023E8A] px-5 py-2.5 text-sm font-medium text-white shadow mt-4 mb-8"
                 type="submit"
                 id="check_code"
-                onClick={checkCode}
+                onClick={() => {
+                  setShowNext(true);
+                }}
               >
                 {translationList?.next}
               </button>
             </div>
           )}
-          {showIosUdid && (
+          {showNext && (
             <div id="ios_udid">
               <h3 className="text-xl font-semibold">Device UDID</h3>
               <input
@@ -190,52 +112,35 @@ function Claim({ searchParams }) {
                 className="block text-center w-[100%] rounded-md bg-primary hover:bg-[#023E8A] px-5 py-2.5 text-sm font-medium text-white shadow mt-4 mb-8"
                 type="submit"
                 id="submit_ios_udid"
-                onSubmit={submitIosUdid}
+                onClick={submitIosUdid}
+                disabled={workingOnIt}
               >
-                {translationList?.next}
+                {workingOnIt ? "Checking" : !error ? translationList?.next : "Retry"}
               </button>
             </div>
           )}
-          {showICloudUnlock && (
-            <div id="icloud_unlock">
-              <h3 className="text-xl font-semibold">Device Serial Number</h3>
-              <input
-                className="rounded-lg border-gray-200 text-sm placeholder-gray-400 focus:z-10 bg-gray-100 p-1.5 w-full"
-                placeholder="Serial Number"
-                type="serial_number"
-                id="serial_number"
-                value={serialNumber}
-                onChange={(e) => setSerialNumber(e.target.value)}
-              />
-              <input
-                className="rounded-lg border-gray-200 text-sm placeholder-gray-400 focus:z-10 bg-gray-100 p-1.5 w-full"
-                placeholder="Email"
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <button
-                className="block text-center w-[100%] rounded-md bg-primary hover:bg-[#023E8A] px-5 py-2.5 text-sm font-medium text-white shadow mt-4 mb-8"
-                type="submit"
-                id="submit_serial_number"
-                onSubmit={submitSerialNumber}
-              >
-                Next
-              </button>
+          {showNext && (
+            <div id="ready">
+              <h3 className="text-xl font-semibold">You're all set!</h3>
+              <p>
+                {translationList?.all_set}
+              </p>
             </div>
           )}
-          {successMessage && (
-            <div id="success">
-              <h3 className="text-xl font-semibold">Success</h3>
-              <p id="success_message">{successMessage}</p>
-            </div>
+          {error && (
+            <p className="text-red-500">
+              {translationList?.error}: {error}
+            </p>
           )}
-          {errorMessage && (
-            <div id="error">
-              <h3 className="text-xl font-semibold">Error</h3>
-              <p id="error_message">{errorMessage}</p>
-            </div>
+          {showNext && (
+            <span>
+              <p className="text-sm">
+                To get your UDID, please go <span className="underline text-blue-600"><Link href="https://udid.starfiles.co">here</Link></span>
+              </p>
+              <p className="text-sm">
+                You are claiming the code: <strong>{codeInput}</strong>
+              </p>
+            </span>
           )}
         </div>
       </div>
